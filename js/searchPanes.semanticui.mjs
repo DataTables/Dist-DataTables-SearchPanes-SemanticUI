bf3e0fcdf9c3ba5a4b5475d78a4cb554095816fc
Allan Jardine <allan@datatables.net>
2022-10-10 19:51:28 +0000

    Add support for mjs files (ES Modules)

---
+++ js/searchPanes.semanticui.mjs
@@ -0,0 +1,32 @@
+
+/*! semantic ui integration for DataTables' SearchPanes
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-se';
+import DataTable from 'datatables.net-searchpanes';
+
+
+$.extend(true, DataTable.SearchPane.classes, {
+    buttonGroup: 'right floated ui buttons column',
+    disabledButton: 'disabled',
+    narrowSearch: 'dtsp-narrowSearch',
+    narrowSub: 'dtsp-narrow',
+    paneButton: 'basic ui',
+    paneInputButton: 'circular search link icon',
+    topRow: 'row dtsp-topRow'
+});
+$.extend(true, DataTable.SearchPanes.classes, {
+    clearAll: 'dtsp-clearAll basic ui button',
+    collapseAll: 'dtsp-collapseAll basic ui button',
+    disabledButton: 'disabled',
+    showAll: 'dtsp-showAll basic ui button'
+});
+// This override is required for the integrated search Icon in sematic ui
+DataTable.SearchPane.prototype._searchContSetup = function () {
+    $('<i class="' + this.classes.paneInputButton + '"></i>').appendTo(this.dom.searchCont);
+};
+
+
+export default DataTable;
